6919c8879677447a3de1aaaae4af59d1354b0bb9
ashish-2210-ashish <ashishsuresh2210@gmail.com>
2025-03-12 15:18:06 +0530

    commit 3.0

--- Rider_Co/src/main/java/com/example/Rider_Co/utils/JwtUtil.java
+++ Rider_Co/src/main/java/com/example/Rider_Co/utils/JwtUtil.java
@@ -24,28 +24,28 @@ public class JwtUtil {
         return Jwts.builder()
                 .setSubject(username)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // 1 hour expiry
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 30))
                 .signWith(getSigningKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
 
-    public String extractUsername(String token) {
-        return getClaims(token).getSubject();
-    }
-
-    public boolean isTokenValid(String token) {
-        return extractUsername(token) != null && !isTokenExpired(token);
-    }
-
-    private boolean isTokenExpired(String token) {
-        return getClaims(token).getExpiration().before(new Date());
-    }
-
-    private Claims getClaims(String token) {
-        return Jwts.parserBuilder()
-                .setSigningKey(getSigningKey())
-                .build()
-                .parseClaimsJws(token)
-                .getBody();
-    }
+//    public String extractUsername(String token) {
+//        return getClaims(token).getSubject();
+//    }
+//
+//    public boolean isTokenValid(String token) {
+//        return extractUsername(token) != null && !isTokenExpired(token);
+//    }
+//
+//    private boolean isTokenExpired(String token) {
+//        return getClaims(token).getExpiration().before(new Date());
+//    }
+//
+//    private Claims getClaims(String token) {
+//        return Jwts.parserBuilder()
+//                .setSigningKey(getSigningKey())
+//                .build()
+//                .parseClaimsJws(token)
+//                .getBody();
+//    }
 }
